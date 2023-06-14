8e9d0152682a31307f310aca3be51c6c52694908
Danish Zahid <danishst.johns@gmail.com>
2023-06-14 21:09:39 +0530

    Done with SignUpPage

--- src/pages/Auth/SignUpPage.jsx
+++ src/pages/Auth/SignUpPage.jsx
@@ -1,18 +1,75 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import styles from './SignUpPage.module.css';
-
+import { useContext, useState } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
 
 export const SignUpPage = () =>{
+  
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const {user, setUser} = useContext(AuthContext);
+
+
+    const signUpHandler = async () =>{
+        try{
+            const response = await fetch("/api/auth/signup",{
+                method:"POST",
+                headers:{
+                    "Content-Type":"application/json"
+                },
+                body:JSON.stringify({
+                    name,
+                    email,
+                    password,
+                })
+            }
+            )
+            const responseData = await response.json();
+            console.log(responseData);
+            setUser({user:responseData.createdUser, token:responseData.encodedToken})
+            navigate("/products");
+          
+          }
+          catch(e){
+              console.log(e)
+          }
+    }
     return(
         <>
             <div className={styles.signupPage}>
       <h2>Sign Up</h2>
-      <form onSubmit={()=>{}}>
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <input type="password" placeholder="Confirm Password" />
+      <form onSubmit={(e)=>{
+        e.preventDefault();
+        signUpHandler();
+      }}>
+        <input onChange={
+          (e)=>{
+            setName(e.target.value)
+          }
+        }
+        value={name}
+         type="text" placeholder="Name" />
+        <input 
+        onChange={
+          (e)=>{
+            setEmail(e.target.value)
+          }
+        }
+        value={email}
+        type="email" placeholder="Email" />
+        <input
+        onChange={
+          (e)=>{
+            setPassword(e.target.value)
+          }
+        }
+        value={password}
+        type="password" placeholder="Password" />
+        {/* <input type="password" placeholder="Confirm Password" /> */}
         <button type="submit">Sign Up</button>
       </form>
       <p>Already have an account? <Link to="/login">Login</Link></p>
--- src/pages/Profile/UserProfileComponent.jsx
+++ src/pages/Profile/UserProfileComponent.jsx
@@ -1,5 +1,14 @@
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+
 export const UserProfile = () =>{
+    const {user,setUser} = useContext(AuthContext);
     return(
-        <><h1>User Profile Page</h1></>
+        <><h1>Hello {user.user.name}</h1>
+        <h2>Email: {user.user.email}</h2>
+        <button onClick={()=>{
+            setUser({user:null, token:null})
+        }} >Logout</button>
+        </>
     )
 }
