5eca8d5db0e88f3d0ec4bf7dcec8c2831b68ec04
danishzahid <danishst.johns@gmail.com>
2023-06-17 01:28:46 +0530

    Done till addToCart

--- src/components/CartPage/CartPage.jsx
+++ src/components/CartPage/CartPage.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import CartProductComponent from '../CartProductComponent/CartProductComponent';
-import styles from './CartPage.module.css';
+import React from "react";
+import CartProductComponent from "../CartProductComponent/CartProductComponent";
+import styles from "./CartPage.module.css";
 
 const CartPage = ({ cart }) => {
   return (
@@ -8,15 +8,17 @@ const CartPage = ({ cart }) => {
       <div className={styles.cartProductListing}>
         <h2 className={styles.heading}>Cart Items</h2>
         {cart.map((item) => {
-            return (
-                <CartProductComponent
-                key={item.id}
-                image={item.image}
-                name={item.name}
-                price={item.price}
-                quantity={item.quantity}
-              />
-        )})}
+          return (
+            <CartProductComponent
+              key={item.id}
+              image={item.image}
+              name={item.name}
+              price={item.price}
+              qty={item.qty}
+              id={item._id}
+            />
+          );
+        })}
       </div>
       <div className={styles.checkout}>
         <h2 className={styles.heading}>Checkout</h2>
--- src/reducers/DataReducer.jsx
+++ src/reducers/DataReducer.jsx
@@ -1,31 +1,40 @@
-import { createContext, useContext, useEffect, useReducer, useState } from "react";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
 
 export const initialState = {
-    categories:[],
-    productList:[],
-    ratingFilter: null,
-    categoryFilter : [],
-    priceFilter: null,
-}
+  categories: [],
+  productList: [],
+  ratingFilter: null,
+  categoryFilter: [],
+  priceFilter: null,
+  cart: [],
+};
 
 export const DataReducer = (state, action) => {
-    switch (action.type) {
-        case "INITIALIZE_CATEGORIES":
-            return {...state, categories:action.payload}
-            break;
-        case "INITIALIZE_PRODUCTS":
-            return {...state, productList:action.payload}
-            break;
-        case "SET_RATING":
-                return { ...state, ratingFilter: action.payload };
-            break
-        case "SET_CATEGORYFILTER":
-                return { ...state, categoryFilter: action.payload };
-            break
-        case "SET_PRICEFILTER":
-                return { ...state, priceFilter: action.payload };
-            break
-        default:
-            break;
-    }
-}
+  switch (action.type) {
+    case "INITIALIZE_CATEGORIES":
+      return { ...state, categories: action.payload };
+      break;
+    case "INITIALIZE_PRODUCTS":
+      return { ...state, productList: action.payload };
+      break;
+    case "SET_RATING":
+      return { ...state, ratingFilter: action.payload };
+      break;
+    case "SET_CATEGORYFILTER":
+      return { ...state, categoryFilter: action.payload };
+      break;
+    case "SET_PRICEFILTER":
+      return { ...state, priceFilter: action.payload };
+      break;
+    case "SET_CART":
+      return { ...state, cart: action.payload };
+    default:
+      break;
+  }
+};
