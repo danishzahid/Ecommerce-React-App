d56976c4228bb902d3cd2c114f076ccabff391fa
Mobashir Farhan <farhanmobashir007@gmail.com>
2023-06-16 15:34:34 +0530

    testing from mob comp

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,22 +1,20 @@
-import "./Home.css"
-import heroImage from "../../images/header-image.jpg"
-import CategoryCardList from "../../components/CategoryCardList"
+import "./Home.css";
+import heroImage from "../../images/header-image.jpg";
+import CategoryCardList from "../../components/CategoryCardList";
 
-
-export const Home = () =>{
-    return(
-        <>
-            <div className="header">
-                <div className="heading">
-                    <h1>This is my heading</h1>
-                    <button className="btn">Shop Now</button>
-                </div>
-                <div className="image">
-                    <img src={heroImage} alt="Example Image" />
-                </div>  
-            </div>
-            <CategoryCardList />
-
-        </>
-    )
-}
+export const Home = () => {
+  return (
+    <>
+      <div className="header">
+        <div className="heading">
+          <h1>Heding up to 3 lines will be here</h1>
+          <button className="btn">Shop Now</button>
+        </div>
+        <div className="image">
+          <img src={heroImage} alt="Example Image" />
+        </div>
+      </div>
+      <CategoryCardList />
+    </>
+  );
+};
